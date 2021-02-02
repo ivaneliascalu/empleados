fe47e3b79be8e91a2c191474a48c87133965c450
Ivan Elias Carrasco Lupinta <ivaneliascalu@gmail.com>
2021-02-02 12:22:31 -0500

    XD

---
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+
+@Component({
+  selector: 'app-create-empleados',
+  templateUrl: './create-empleados.component.html',
+  styleUrls: ['./create-empleados.component.css']
+})
+
+export class CreateEmpleadosComponent implements OnInit {
+  createEmpleado: FormGroup;
+  submitted= false;
+  loading= false;
+  id: string | null;
+  titulo= 'Agregar Empleado';
+  
+  constructor(private fb: FormBuilder,
+              private _empleadoService: EmpleadoService,
+              private router: Router,
+              private toastr: ToastrService,
+              private aRoute: ActivatedRoute) {
+    this.createEmpleado=this.fb.group({
+      nombre: ['',Validators.required],
+      apellidos: ['',Validators.required],
+      documento: ['',Validators.required],
+      salario: ['',Validators.required]
+    })
+
+    this.id=this.aRoute.snapshot.paramMap.get('id');
+    console.log(this.id)
+   }
+
+  ngOnInit(): void {
+    this.esEditar();
+  }
+  
+  agregarEmpleado(){
+    this.submitted=true
+    if (this.createEmpleado.invalid) {
+      return;
+    }
+    const empleado: any = {
+      nombre: this.createEmpleado.value.nombre,
+      apellidos: this.createEmpleado.value.apellidos,
+      documento: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    this.loading=true;
+    this._empleadoService.agregarEmpleado(empleado).then(()=>{
+      this.toastr.success('empleado registrado con éxito!', 'Empleado Registrado',{
+        positionClass: 'toast-bottom-right'
+      });
+      this.loading=false;
+      this.router.navigate(['/list-empleados']);
+    }).catch(error=>{
+      console.error();
+      this.loading=false;
+    })
+  }
+
+  agregarEditarEmpleado(){
+    this.submitted=true
+    if (this.createEmpleado.invalid) {
+      return;
+    }
+    if (this.id === null) {
+      this.agregarEmpleado();
+    } else {
+      this.editarEmpleado(this.id);
+    }
+  }
+
+  editarEmpleado(id: string) {
+    const empleado: any = {
+      nombre: this.createEmpleado.value.nombre,
+      apellidos: this.createEmpleado.value.apellidos,
+      documento: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    this.loading=true;
+    //llamando servicio
+    this._empleadoService.actualizarEmpleado(id, empleado).then(()=>{
+      this.loading=false;
+      this.toastr.info('empleado modificado con éxito!', 'Empleado Modificado',{
+        positionClass: 'toast-bottom-right'
+      });
+      this.router.navigate(['/list-empleados']);
+    })
+  }
+
+  esEditar(){
+    if (this.id!==null) {
+      this.titulo='Editar Empleado'
+      this._empleadoService.obtenerEmpleado(this.id).subscribe(data=>{
+        this.createEmpleado.setValue({
+          nombre: data.payload.data()['nombre'],
+          apellidos: data.payload.data()['apellidos'],
+          documento: data.payload.data()['documento'],
+          salario: data.payload.data()['salario']
+        })
+      })
+    }
+  }
+}
